ccd1578ba3a574cd6d43ba4deecebeb532f52ed4
Greg Tatum <tatum.creative@gmail.com>
2017-11-13 09:19:47 -0600

    Remove files from master

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Console from './Console';
-
-console.time("render")
-ReactDOM.render(<Console mutableMessages={[]} />, document.getElementById('root'));
-console.timeEnd("render")
